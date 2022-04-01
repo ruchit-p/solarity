ef97e9fdcf7792e8983fbf7f84824c43cbe5bd61
Ruchit Patel <rpate231@depaul.edu>
2022-03-31 21:44:47 -0400

    Added some description about the business and contact info

--- routes/about.js
+++ routes/about.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 /* GET about page. */
 router.get("/", function (req, res, next) {
-  res.render("about", { title: "About SampleApp" });
+  res.render("about", { title: "About Solarity" });
 });
 module.exports = router;
--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 /* GET contact page. */
 router.get("/", function (req, res, next) {
-  res.render("contact", { title: "Contact SampleApp" });
+  res.render("contact", { title: "Contact Us" });
 });
 module.exports = router;
