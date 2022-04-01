6f99b6f8b1adc8fb5156526db3f4b1be0af76a7e
Ruchit Patel <rpate231@depaul.edu>
2022-03-31 21:19:41 -0400

    Created website with aboout and contact pages

---
+++ routes/about.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+/* GET about page. */
+router.get("/", function (req, res, next) {
+  res.render("about", { title: "About SampleApp" });
+});
+module.exports = router;
