e982e6a0dd4e185b989ee3e1a3ccbf8d0a29d5e4
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2022-05-05 16:43:09 -0400

    Fixed homepage checkbox in edit page -> products

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+
 router.get("/", function (req, res, next) {
   let query =
     "SELECT product_id, productname, prodimage, category_id, supplier_id, prodprice, status, quantity FROM product WHERE homepage = true";
