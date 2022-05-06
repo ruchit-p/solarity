0319c48c662069365b41069a13f3225957a322a6
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2022-05-05 23:22:52 -0400

    Completed report menus

--- routes/report.js
+++ routes/report.js
@@ -1,22 +1,48 @@
 var express = require("express");
 var router = express.Router();
 
-
 /* GET report page. */
 router.get("/", function (req, res, next) {
   res.render("report/reportmenu");
 });
 
 router.get("/cust", function (req, res, next) {
-  res.render("report/custlist");
+  let query =
+    "SELECT customer_id, firstname, lastname, email, address1, city, state, zip, username FROM customer";
+  // execute query
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render("error");
+    }
+    res.render("report/custlist", { allrecs: result });
+  });
 });
 
 router.get("/prod", function (req, res, next) {
-  res.render("report/prodlist");
+  let query =
+    "SELECT product_id, productname, supplier_id, category_id, prodprice, status, quantity, homepage FROM product";
+  // execute query
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render("error");
+    }
+    res.render("report/prodlist", { allrecs: result });
+  });
 });
 
 router.get("/sale", function (req, res, next) {
-  res.render("report/salelist");
+  let query =
+    "SELECT order_id, customer_id, saledate, customernotes, paymentstatus, authorizationnum FROM saleorder";
+  // execute query
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render("error");
+    }
+    res.render("report/salelist", { allrecs: result });
+  });
 });
 
 module.exports = router;
