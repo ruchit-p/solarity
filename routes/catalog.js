05d27e6d9fa44e52e5fc50c7835eb4b6f1ecf9bc
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2022-05-14 00:56:37 -0400

    Added new promo images

--- routes/catalog.js
+++ routes/catalog.js
@@ -42,4 +42,40 @@ router.post("/add", function (req, res, next) {
   res.redirect("/catalog/cart");
 });
 
+// ==================================================
+// Route to remove an item from the cart
+// ==================================================
+router.post("/remove", function (req, res, next) {
+  // Find the element index of the product_id that needs to be removed
+  var n = req.session.cart.indexOf(req.body.product_id);
+  // Remove element from cart and quantity arrays
+  req.session.cart.splice(n, 1);
+  req.session.qty.splice(n, 1);
+  res.redirect("/catalog/cart");
+});
+
+// ==================================================
+// Route to show shopping cart
+// ==================================================
+router.get("/cart", function (req, res, next) {
+  if (!Array.isArray(req.session.cart) || !req.session.cart.length) {
+    res.render("cart", { cartitems: 0 });
+  } else {
+    let query =
+      "SELECT product_id, productname, prodimage, supplier_id, category_id, dimensions, prodprice, status FROM product WHERE product_id IN (" +
+      req.session.cart +
+      ") order by find_in_set(product_id, '" +
+      req.session.cart +
+      "');";
+    // execute query
+    db.query(query, (err, result) => {
+      if (err) {
+        res.render("error");
+      } else {
+        res.render("cart", { cartitems: result, qtys: req.session.qty });
+      }
+    });
+  }
+});
+
 module.exports = router;
